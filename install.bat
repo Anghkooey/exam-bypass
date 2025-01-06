d094afb450713e5e9c59118e7b6ed3106adbf7f5
user-sspmynxdvb <pewfellowmnemonic@gmail.com>
2025-01-06 17:15:10 +0100

    added install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Launch Chrome and open two tabs in the same window.
+:: The first tab will navigate to the Chrome Web Store page to install Tampermonkey.
+:: The second tab will navigate to the direct link to the user script for installation in Tampermonkey.
+
+start chrome "https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo" "https://github.com/Anghkooey/exam-bypass/raw/main/exam.user.js"
